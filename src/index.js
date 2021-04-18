d0bd45056cbb0392092d2aa5c91e86e9c0cb57d9
MarieAnge5 <77550571+MarieAnge5@users.noreply.github.com>
2021-04-18 19:34:09 +0200

    added the axios

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import Search from "./Search";
 
 import "./App.css";
