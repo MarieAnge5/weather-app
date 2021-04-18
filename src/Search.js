5a77df4ca6aa371aa170d597b9002e31e27c6b14
MarieAnge5 <77550571+MarieAnge5@users.noreply.github.com>
2021-04-18 13:33:06 +0200

    Initial commit

---
+++ src/Search.js
@@ -0,0 +1,60 @@
+import React , { useState } from "react";
+import axios from "axios";
+
+
+export default function Search() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
+  const [message, setMessage] = useState(""); 
+
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+     description: response.data.weather[0].description
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "3c8fa277b142efb1f38be2fd365120be";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
+    setMessage(`${city}`);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" placeholder="Enter a city" onChange={updateCity} />
+      <button type="submit">Search</button>
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+            <h2>{message}</h2>
+          <li>Temperature:{Math.round(weather.temperature)}°C</li>
+          <li>Description:{weather.description}</li>
+          <li>Humidity:{weather.humidity}%</li>
+          <li>Wind:{weather.wind}km/hr</li>
+          <li>
+            <img src={weather.icon} alt="Weather Icon" />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import Search from "./Search";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="weather-app-wrapper">
+        <div className="weather-app">
+   <h1>Sunday, 19:34pm</h1>
+          <Search />
+          <br />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
